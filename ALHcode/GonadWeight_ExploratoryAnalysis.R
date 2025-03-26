ecd2df4e73d3c0352116ed5f9bfa51c3486114cf
“abigailhost” <abigailhost7@gmail.com>
2025-03-26 11:27:53 -0800

    edits to grams/mm for body size PC score and resulting AIC analysis

---
+++ ALHcode/GonadWeight_ExploratoryAnalysis.R
@@ -0,0 +1,50 @@
+rm(list = ls())
+DataSet <- read.csv("ALHcode/AIC/upperriver_bodycomp_all.csv")
+str(DataSet)
+DataSet$SpawnStatus <- as.factor(DataSet$SpawnStatus)
+DataSet$Gonad_Wt <- as.numeric(DataSet$Gonad_Wt)
+str(DataSet)
+max(DataSet$Gonad_Wt, na.rm = TRUE)
+which(DataSet$Gonad_Wt == max(DataSet$Gonad_Wt, na.rm = TRUE))
+DataSet <- DataSet[-146,] #eliminates the maximum 6.0 value that seems very out of place for weight
+DataSet$Gonad_Wt_g <- (DataSet$Gonad_Wt*1000)
+
+
+# Filter the data for SpawnStatus 0 (pre-spawn) and 2 (post-spawn)
+pre_spawn_data <- DataSet[DataSet$SpawnStatus == 0, ]
+partialpost_spawn_data <- DataSet[DataSet$SpawnStatus %in% c(1, 2), ]
+post_spawn_data <- DataSet[DataSet$SpawnStatus == 2, ]
+
+str(post_spawn_data)
+mean(post_spawn_data$Gonad_Wt_g, na.rm = T) #mean weight is 20.66667 grams
+mean(pre_spawn_data$Gonad_Wt_g, na.rm = T) #mean weight is 137.3151 grams
+mean(partialpost_spawn_data$Gonad_Wt_g, na.rm = T) #mean weight is 35.375 grams
+
+# Create histograms in base R
+par(mfrow = c(1, 3))  # Arrange plots in one row and two columns
+
+# Histogram for pre-spawn (SpawnStatus == 0)
+hist(pre_spawn_data$Gonad_Wt_g, 
+     main = "Pre-Spawn Gonad Weight", 
+     xlab = "Gonad Weight (g)", 
+     col = "lightblue", 
+     border = "black", 
+     breaks = 20)  # Adjust 'breaks' as needed
+
+hist(partialpost_spawn_data$Gonad_Wt_g, 
+     main = "Partial and Post-Spawn Gonad Weight", 
+     xlab = "Gonad Weight (g)", 
+     col = "lightgreen", 
+     border = "black", 
+     breaks = 20)
+
+# Histogram for post-spawn (SpawnStatus == 2)
+hist(post_spawn_data$Gonad_Wt_g, 
+     main = "Post-Spawn Gonad Weight", 
+     xlab = "Gonad Weight (g)", 
+     col = "salmon", 
+     border = "black", 
+     breaks = 20)  # Adjust 'breaks' as needed
+
+# Reset the plotting layout to default (single plot)
+par(mfrow = c(1, 1))
