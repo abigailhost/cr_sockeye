96979e4ecb59eed3559f85b181bcf599b946780f
“abigailhost” <abigailhost7@gmail.com>
2025-03-28 16:50:24 -0800

    changes to fecundity markdown, changes to upperriver data file .csv

--- ALHcode/GonadWeight_ExploratoryAnalysis.R
+++ ALHcode/GonadWeight_ExploratoryAnalysis.R
@@ -12,13 +12,13 @@ DataSet$Gonad_Wt_g <- (DataSet$Gonad_Wt*1000)
 
 # Filter the data for SpawnStatus 0 (pre-spawn) and 2 (post-spawn)
 pre_spawn_data <- DataSet[DataSet$SpawnStatus == 0, ]
-partialpost_spawn_data <- DataSet[DataSet$SpawnStatus %in% c(1, 2), ]
+partial_spawn_data <- DataSet[DataSet$SpawnStatus == 1, ]
 post_spawn_data <- DataSet[DataSet$SpawnStatus == 2, ]
 
 str(post_spawn_data)
 mean(post_spawn_data$Gonad_Wt_g, na.rm = T) #mean weight is 20.66667 grams
 mean(pre_spawn_data$Gonad_Wt_g, na.rm = T) #mean weight is 137.3151 grams
-mean(partialpost_spawn_data$Gonad_Wt_g, na.rm = T) #mean weight is 35.375 grams
+mean(partial_spawn_data$Gonad_Wt_g, na.rm = T) #mean weight is 31.5 grams
 
 # Create histograms in base R
 par(mfrow = c(1, 3))  # Arrange plots in one row and two columns
@@ -31,7 +31,7 @@ hist(pre_spawn_data$Gonad_Wt_g,
      border = "black", 
      breaks = 20)  # Adjust 'breaks' as needed
 
-hist(partialpost_spawn_data$Gonad_Wt_g, 
+hist(partial_spawn_data$Gonad_Wt_g, 
      main = "Partial and Post-Spawn Gonad Weight", 
      xlab = "Gonad Weight (g)", 
      col = "lightgreen", 
@@ -55,7 +55,7 @@ par(mfrow = c(1, 1))
 
 # Get the min and max range for Gonad_Wt_g across all groups
 all_data <- c(pre_spawn_data$Gonad_Wt_g, 
-              partialpost_spawn_data$Gonad_Wt_g, 
+              partial_spawn_data$Gonad_Wt_g, 
               post_spawn_data$Gonad_Wt_g)
 
 x_range <- range(all_data, na.rm=T)  # Get min and max values
@@ -73,7 +73,7 @@ hist(pre_spawn_data$Gonad_Wt_g[pre_spawn_data$Sex == "M"],
      xlim = x_range)
 
 # Males - Partial and Post-Spawn
-hist(partialpost_spawn_data$Gonad_Wt_g[partialpost_spawn_data$Sex == "M"], 
+hist(partial_spawn_data$Gonad_Wt_g[partialpost_spawn_data$Sex == "M"], 
      main = "Males: Partial/Post-Spawn Gonad Weight", 
      xlab = "Gonad Weight (g)", 
      col = "lightgreen", 
@@ -100,7 +100,7 @@ hist(pre_spawn_data$Gonad_Wt_g[pre_spawn_data$Sex == "F"],
      xlim = x_range)
 
 # Females - Partial and Post-Spawn
-hist(partialpost_spawn_data$Gonad_Wt_g[partialpost_spawn_data$Sex == "F"], 
+hist(partial_spawn_data$Gonad_Wt_g[partialpost_spawn_data$Sex == "F"], 
      main = "Females: Partial/Post-Spawn Gonad Weight", 
      xlab = "Gonad Weight (g)", 
      col = "lightgreen", 
